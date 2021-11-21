a1984cd53450345e5f74de22a2f34d945a28e72e
ScienceTechWorks <sciencetechworks@gmail.com>
2021-11-21 18:42:42 +0100

    MockMvc.. Testing an MVC Controller.

--- src/test/java/com/stw/spring5_recipe_app/controllers/IndexControllerTest.java
+++ src/test/java/com/stw/spring5_recipe_app/controllers/IndexControllerTest.java
@@ -15,7 +15,12 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 import static org.mockito.Mockito.*;
-
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.web.servlet.function.ServerResponse.status;
 /**
  *
  * @author Usuario
@@ -79,6 +84,14 @@ public class IndexControllerTest {
         verify(model,times(1)).addAttribute(eq("recipes"),argumentCaptor.capture());
         Set<Recipe> setInController = argumentCaptor.getValue();
         assertEquals(2,setInController.size());
+    }
+    
+    @Test
+    public void testMockMVC() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+        
+        mockMvc.perform(get("/")).andExpect(status().isOk()).
+                andExpect(view().name("index"));
     }
     
 }
