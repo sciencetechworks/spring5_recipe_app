76ce68fc4de5a9431b0db81625f7a98a87298a90
ScienceTechWorks <sciencetechworks@gmail.com>
2021-11-25 18:11:33 +0100

    Enumeration Dropdown for Difficulty.

--- src/main/java/com/stw/spring5_recipe_app/controllers/RecipeController.java
+++ src/main/java/com/stw/spring5_recipe_app/controllers/RecipeController.java
@@ -28,8 +28,7 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
     
-    @GetMapping
-    @RequestMapping("/recipe/{id}/show")
+    @GetMapping("/recipe/{id}/show")
     public String showById(@PathVariable String id, Model model){
         
         log.debug("Recipe controller called.");
@@ -39,16 +38,14 @@ public class RecipeController {
         return "recipe/show";
     }  
     
-    @GetMapping
-    @RequestMapping("recipe/new")
+    @GetMapping("recipe/new")
     public String newRecipe(Model model){
         model.addAttribute("recipe", new RecipeCommand());
 
         return "recipe/recipeform";
     }
     
-    @GetMapping
-    @RequestMapping("recipe/{id}/update")
+    @GetMapping("recipe/{id}/update")
     public String updateRecipe(@PathVariable String id, Model model){
         model.addAttribute("recipe", recipeService.getRecipeById(Long.valueOf(id)));
         return  "recipe/recipeform";
@@ -61,8 +58,7 @@ public class RecipeController {
         return "redirect:/recipe/" + savedCommand.getId()+"/show";
     }
     
-    @GetMapping
-    @RequestMapping("recipe/{id}/delete")
+    @GetMapping("recipe/{id}/delete")
     public String deleteById(@PathVariable String id){
 
         log.debug("Deleting id: " + id);
