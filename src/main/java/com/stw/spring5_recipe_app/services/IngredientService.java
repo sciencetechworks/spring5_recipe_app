8b2d060bcf3977bdb922a7b9fa1a092d73c7947d
ScienceTechWorks <sciencetechworks@gmail.com>
2021-11-25 17:50:59 +0100

    Delete Ingredient.

--- src/main/java/com/stw/spring5_recipe_app/services/IngredientService.java
+++ src/main/java/com/stw/spring5_recipe_app/services/IngredientService.java
@@ -14,4 +14,6 @@ public interface IngredientService {
     IngredientCommand findByRecipeIdAndIngredientId(Long recipeId, Long ingredientId);
     
      IngredientCommand saveIngredientCommand(IngredientCommand command);
+     
+     void deleteById(Long recipeId, Long idToDelete);
 }
