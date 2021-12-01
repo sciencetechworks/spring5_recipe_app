ccab47c2443cd5cda5c59424668f9550dc989645
ScienceTechWorks <sciencetechworks@gmail.com>
2021-12-01 16:36:28 +0100

    Customize error messages.

--- src/test/java/com/stw/spring5_recipe_app/controllers/RecipeControllerTest.java
+++ src/test/java/com/stw/spring5_recipe_app/controllers/RecipeControllerTest.java
@@ -138,7 +138,7 @@ public class RecipeControllerTest {
         mockMvc.perform(post("/recipe")
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                 .param("id", "")
-                .param("description","")
+                .param("cookTime","3000")
 
         )
                 .andExpect(status().isOk())
